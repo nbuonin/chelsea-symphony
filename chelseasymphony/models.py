1dbabc885695bd15741f1fb5c037f929029c8c5b
Nick Buonincontri <nbuonin@gmail.com>
2018-10-23 23:22:07 -0400

    Initial concert model

--- chelseasymphony/models.py
+++ chelseasymphony/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.postgres.fields import DateTimeRangeField
+from django.contrib.postgres.forms import RangeWidget
 
+from modelcluster.fields import ParentalKey
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel \
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, \
     InlinePanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.models import register_snippet
@@ -14,49 +17,79 @@ class Home(Page):
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+'
+        related_name='banner_image'
     )
 
     content_panels = Page.content_panels + [
         ImageChooserPanel('banner_image')
     ]
 
-    parent_page_types = [chelseasymphony.Concert]
-    subpage_types = []
+    subpage_types = ['chelseasymphony.ConcertIndex']
+
+
+class PerformanceDate(models.Model):
+    concert = ParentalKey(
+        'Concert',
+        on_delete=models.CASCADE,
+        related_name='performance_date'
+    )
+    date = models.DateTimeField()
+
+    panels = [
+        FieldPanel('date')
+    ]
 
 
 class Concert(Page):
-    title = RichTextField()
-    description =
-    venue =
-    performances =
-    image =
-    dates =
+    description = RichTextField()
+    venue = RichTextField()
+    #performances =
+    concert_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='concert_image'
+    )
 
     @property
     def conductors():
         pass
 
+    content_panels = Page.content_panels + [
+        FieldPanel('description'),
+        FieldPanel('venue'),
+        ImageChooserPanel('concert_image'),
+        InlinePanel('performance_date', label="Performance Dates")
+    ]
 
-@register_snippet
-class Performance(models.Model):
-    pass
-
-
-@register_snippet
-class Performer(models.Model):
-    pass
-
-
-@register_snippet
-class Composition(models.Model):
-    pass
+    parent_page_types = ['chelseasymphony.ConcertIndex']
 
 
-class Person(Page):
-    pass
+class ConcertIndex(Page):
+    parent_page_types = ['chelseasymphony.Home']
+    subpage_types = ['chelseasymphony.Concert']
 
 
-@register_snippet
-class Instrument(models.Model):
-    pass
+# @register_snippet
+# class Performance(models.Model):
+    # pass
+
+
+# @register_snippet
+# class Performer(models.Model):
+    # pass
+
+
+# @register_snippet
+# class Composition(models.Model):
+    # pass
+
+
+# class Person(Page):
+    # pass
+
+
+# @register_snippet
+# class Instrument(models.Model):
+    # pass
