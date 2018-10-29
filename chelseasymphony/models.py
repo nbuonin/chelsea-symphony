3df54aaa79d92b3f08f56bba3f27e965eae22481
Nick Buonincontri <nbuonin@gmail.com>
2018-10-28 21:54:55 -0400

    Initial data modeling and migrations

--- chelseasymphony/models.py
+++ chelseasymphony/models.py
@@ -1,14 +1,22 @@
+from django import forms
 from django.db import models
 from django.contrib.postgres.fields import DateTimeRangeField
 from django.contrib.postgres.forms import RangeWidget
 
-from modelcluster.fields import ParentalKey
-from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, \
-    InlinePanel
+    InlinePanel, StreamFieldPanel, PageChooserPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+from wagtail.snippets.blocks import SnippetChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.models import register_snippet
+
+from wagtailautocomplete.edit_handlers import AutocompletePanel
+
+from modelcluster.models import ClusterableModel
 
 
 class Home(Page):
@@ -16,7 +24,7 @@ class Home(Page):
         'wagtailimages.Image',
         null=True,
         blank=True,
-        on_delete=models.SET_NULL,
+        on_delete=models.PROTECT,
         related_name='banner_image'
     )
 
@@ -24,7 +32,18 @@ class Home(Page):
         ImageChooserPanel('banner_image')
     ]
 
-    subpage_types = ['chelseasymphony.ConcertIndex']
+    subpage_types = [
+        'chelseasymphony.ConcertIndex',
+        'chelseasymphony.PersonIndex'
+    ]
+
+
+class SimplePage(Page):
+    body = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body')
+    ]
 
 
 class PerformanceDate(models.Model):
@@ -33,22 +52,29 @@ class PerformanceDate(models.Model):
         on_delete=models.CASCADE,
         related_name='performance_date'
     )
-    date = models.DateTimeField()
+    date = models.DateTimeField(
+        null=False,
+        blank=False
+    )
 
     panels = [
         FieldPanel('date')
     ]
 
 
+class ConcertIndex(Page):
+    parent_page_types = ['chelseasymphony.Home']
+    subpage_types = ['chelseasymphony.Concert']
+
+
 class Concert(Page):
     description = RichTextField()
     venue = RichTextField()
-    #performances =
     concert_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
         blank=True,
-        on_delete=models.SET_NULL,
+        on_delete=models.PROTECT,
         related_name='concert_image'
     )
 
@@ -60,36 +86,119 @@ class Concert(Page):
         FieldPanel('description'),
         FieldPanel('venue'),
         ImageChooserPanel('concert_image'),
-        InlinePanel('performance_date', label="Performance Dates")
+        InlinePanel('performance_date', label="Performance Dates"),
     ]
 
     parent_page_types = ['chelseasymphony.ConcertIndex']
 
 
-class ConcertIndex(Page):
-    parent_page_types = ['chelseasymphony.Home']
-    subpage_types = ['chelseasymphony.Concert']
+class Performance(Page):
+    composition = models.ForeignKey(
+        'chelseasymphony.Composition',
+        null=False,
+        blank=False,
+        on_delete=models.PROTECT,
+        related_name='+'
+    )
+    conductor = models.ForeignKey(
+        'chelseasymphony.Person',
+        null=False,
+        blank=False,
+        on_delete=models.PROTECT,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        SnippetChooserPanel('composition'),
+        InlinePanel('performer', label='Performers'),
+        SnippetChooserPanel('conductor')
+    ]
+
+    parent_page_types = ['chelseasymphony.Concert']
+    subpage_types = []
 
 
-# @register_snippet
-# class Performance(models.Model):
-    # pass
+class Performer(models.Model):
+    """
+    This is a performer in the sense of an instance of a performance.
+    """
+    performance = ParentalKey(
+        'chelseasymphony.Performance',
+        on_delete=models.CASCADE,
+        related_name='performer'
+    )
+    person = models.ForeignKey(
+        'chelseasymphony.Person',
+        null=False,
+        blank=False,
+        on_delete=models.PROTECT,
+        related_name='+'
+    )
+    instrument = models.ForeignKey(
+        'chelseasymphony.InstrumentModel',
+        null=False,
+        blank=False,
+        on_delete=models.PROTECT,
+        related_name='+'
+    )
+
+    content_panels = [
+        PageChooserPanel('person'),
+        SnippetChooserPanel('instrument')
+    ]
 
 
-# @register_snippet
-# class Performer(models.Model):
-    # pass
+@register_snippet
+class Composition(models.Model):
+    title = RichTextField()
+    composer = models.ForeignKey(
+        'chelseasymphony.Person',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = [
+        SnippetChooserPanel('composer')
+    ]
 
 
-# @register_snippet
-# class Composition(models.Model):
-    # pass
+class Person(Page):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    biography = RichTextField()
+    active_roster= models.BooleanField()
+    headshot = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT,
+        related_name='+'
+    )
+    instrument = ParentalManyToManyField(
+        'chelseasymphony.InstrumentModel',
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('first_name'),
+        FieldPanel('last_name'),
+        FieldPanel('biography'),
+        FieldPanel('active_roster'),
+        ImageChooserPanel('headshot'),
+        # below may need to be a FieldPanel
+        SnippetChooserPanel('instrument')
+    ]
+
+    parent_page_types = ['chelseasymphony.PersonIndex']
 
 
-# class Person(Page):
-    # pass
+class PersonIndex(Page):
+    # The roster page will go here
+    subpage_types = ['chelseasymphony.Person']
 
 
-# @register_snippet
-# class Instrument(models.Model):
-    # pass
+@register_snippet
+class InstrumentModel(models.Model):
+    instrument = models.CharField(max_length=255)
