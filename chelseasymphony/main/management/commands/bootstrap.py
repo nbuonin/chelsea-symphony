afb083ac5e08b9314565243affb2b0dbf25d5dfd
Nick Buonincontri <nbuonin@gmail.com>
2019-07-17 23:45:33 -0400

    In Bootstrap management command, create all the instruments

--- chelseasymphony/main/management/commands/bootstrap.py
+++ chelseasymphony/main/management/commands/bootstrap.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.core.management import CommandError
 from wagtail.core.models import Page
 from chelseasymphony.main.tests.test_models import create_base_site
+from chelseasymphony.main.models import InstrumentModel
 
 
 class Command(BaseCommand):
@@ -12,3 +13,27 @@ class Command(BaseCommand):
             raise CommandError('There already exists content in the database.')
         else:
             create_base_site()
+
+            instrument_names = [
+                'Violin',
+                'Viola',
+                'Cello',
+                'Double Bass',
+                'Harp',
+                'Flute',
+                'Oboe',
+                'Clarinet',
+                'Bassoon',
+                'French Horn',
+                'Trumpet',
+                'Trombone',
+                'Tuba',
+                'Percussion',
+                'Timpani',
+                'Piano',
+                'Harpsichord'
+            ]
+            for instrument in instrument_names:
+                InstrumentModel.objects.create(
+                    instrument=instrument
+                )
