fccef8640184d4fc47e18867d3576b5f2f00ce3a
Nick Buonincontri <nbuonin@gmail.com>
2019-07-20 13:01:36 -0400

    Import script
    Fixes a number of bugs in the import script and in models
    
    Specifically, it fixes problems around programatically creating slugs
    for Person and Performance pages.
    
    It also includes some migrations for changes to models

--- chelseasymphony/main/management/commands/import.py
+++ chelseasymphony/main/management/commands/import.py
@@ -1,27 +1,29 @@
-from wagtail.core.models import Page, PageManager, Orderable, PageQuerySet, Site
-from django.apps import apps
-ContentType = apps.get_model('contenttypes.ContentType')
-from django.core.management.base import BaseCommand
-from django.utils.dateparse import parse_date, parse_datetime
-from django.utils.timezone import make_aware
-from chelseasymphony.main.models import (
-    Home, BasicPage, ConcertDate, ConcertIndex, Concert,
-    Performance, Performer, Composition, Person, PersonIndex,
-    InstrumentModel, BlogPost, BlogIndex, ActiveRosterMusician,
-    Donate
-)
-from wagtail.core.rich_text import RichText
-from wagtail.contrib.redirects.models import Redirect
-from wagtail.images import get_image_model
-WagtailImage = get_image_model()
-
+from io import BytesIO
+import os
 from PIL import Image
 import re
-
-
+from urllib import parse
 import requests
+from chelseasymphony.main.models import (
+    ConcertDate, ConcertIndex, Concert,
+    Performance, Performer, Composition, Person, PersonIndex,
+    InstrumentModel, BlogPost, BlogIndex
+)
+from django.apps import apps
+from django.core.management.base import BaseCommand
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.utils.dateparse import parse_date, parse_datetime
+from django.utils.timezone import make_aware
+from wagtail.contrib.redirects.models import Redirect
+from wagtail.core.rich_text import RichText
+from wagtail.images import get_image_model
+
+WagtailImage = get_image_model()
+ContentType = apps.get_model('contenttypes.ContentType')
+
 
 IMPORT_BASE_URL = 'http://localhost:8080'
+
 
 class Command(BaseCommand):
     help = 'Demo the command'
@@ -71,39 +73,49 @@ class Command(BaseCommand):
         self.blog_posts = [b['node'] for b in bp]
 
         bp_img = requests.get(IMPORT_BASE_URL + '/api/blogpost-image').json()['nodes']
-        self.blog_posts_images = [b['node'] for b in bp]
+        self.blog_posts_images = [b['node'] for b in bp_img]
 
-    def get_concert_dates(self, id):
+    def get_concert_dates(self, cid):
         """Gets concert dates by concert ID"""
-        return [d for d in self.concert_dates if d['nid'] == id]
+        print('cid: ' + str(cid))
+        return [d for d in self.concert_dates if d['nid'] == str(cid)][0]
 
-    def get_concert_performances(self, id):
+    def get_concert_performances(self, nid):
         """Gets concert performances by concert ID"""
-        performances = [p for p in self.concert_performances if p['nid'] == id]
+        performances = [p for p in self.concert_performances if p['nid'] == str(nid)]
         return sorted(performances, key=lambda p: p['program_order'])
 
-    def get_soloists_by_performance_id(self, id):
+    def get_soloists_by_performance_id(self, nid):
         """Gets soloists by performance ID"""
-        return [s for s in self.concert_soloists if s['performance_id'] == id]
+        return [s for s in self.concert_soloists if s['performance_id'] == str(nid)]
 
     def get_concert_photo_by_id(self, nid):
         """
         Gets concert images by node id
         Note: this uses the tightest crop to set the focal point for the image
         """
-        return [p for p in self.concert_photos
-                if (p['nid'] == nid and
-                    p['crop_style_name'] == 'tcs2r_concert_image_0_79')][0]
+        try:
+            return [p for p in self.concert_photos
+                    if (p['nid'] == str(nid) and
+                        p['crop_style_name'] == 'tcs2r_concert_image_0_79')][0]
+        except IndexError:
+            return None
 
     def get_headshot_by_uid(self, uid):
-        return [p for p in self.headshots
-                if (p['uid'] == uid and
-                    p['crop_style_name'] == 'tcs2r_musician_headshot_1_5')][0]
+        try:
+            return [p for p in self.headshots
+                    if (p['uid'] == str(uid) and
+                        p['crop_style_name'] == 'tcs2r_musician_headshot_1_5')][0]
+        except IndexError:
+            return None
 
     def get_blog_img_from_id(self, nid):
-        return [p for p in self.blog_posts_images
-                if (p['nid'] == nid and
-                    p['crop_style_name'] == 'tsc2r_blog_list___homepage_desktop')]
+        try:
+            return [p for p in self.blog_posts_images
+                    if (p['nid'] == str(nid) and
+                        p['crop_style_name'] == 'tsc2r_blog_list___homepage_desktop')][0]
+        except IndexError:
+            return None
 
     def get_wagtail_image(self, url):
         """
@@ -151,14 +163,18 @@ class Command(BaseCommand):
 
         try:
             return Person.objects.get(first_name=first, last_name=last)
+        except Person.MultipleObjectsReturned:
+            # Names are ambigious! Because there are no pk's on composer names
+            # from Drupal, just use the first Person we find
+            return Person.objects.filter(first_name=first, last_name=last).first()
         except Person.DoesNotExist:
             person = Person(
                 first_name=first,
-                last_name=last
+                last_name=last,
+                active_roster=False
             )
-            person.full_clean()
             self.person_idx.add_child(instance=person)
-            person.save_revision.publish()
+            person.save_revision().publish()
             return person
 
     def get_or_create_composition(self, composition, composer):
@@ -193,18 +209,20 @@ class Command(BaseCommand):
             "performance_id" : "366"
         }
         """
-        for perf in this.get_concert_performances(concert.legacy_id):
+        for perf in self.get_concert_performances(concert.legacy_id):
             print('Creating performance for concert ' + concert.title +
                   ': ' + perf['composition'])
             # This assumes that all Person objects are created first
-            conductor = Person.objects.get(legacy_id=perf['conductor_uid'])
+            conductor = None
+            if perf['conductor_uid']:
+                conductor = Person.objects.get(legacy_id=perf['conductor_uid'])
+
             composition = self.get_or_create_composition(
-                perf['composition'], perf['compose'])
-            performance = Performance.objects.create(
+                perf['composition'], perf['composer'])
+            performance = Performance(
                 conductor=conductor,
                 composition=composition
             )
-            performance.full_clean()
             concert.add_child(instance=performance)
             performance.save_revision().publish()
 
@@ -212,19 +230,19 @@ class Command(BaseCommand):
                 date = parse_date(perf_date)
                 concert_date = concert.concert_date.get(date__date=date)
                 performance.performance_date.add(concert_date)
+                performance.save_revision().publish()
 
             for performer in self.\
                     get_soloists_by_performance_id(perf['performance_id']):
                 print('Adding performer: ' + performer['soloist'])
                 soloist = Person.objects.get(legacy_id=performer['uid'])
-                instrument = InstrumentModel.objects.get_or_create(
-                    instrument=performer['instrument]'])
+                instrument, created = InstrumentModel.objects.get_or_create(
+                    instrument=performer['instrument'])
                 Performer.objects.create(
                     performance=performance,
                     person=soloist,
                     instrument=instrument
                 )
-
 
     def create_concert_dates(self, concert):
         """
@@ -235,21 +253,23 @@ class Command(BaseCommand):
         }
         """
         dates = self.get_concert_dates(concert.legacy_id)
-        date_strings = [d.strip() for d in dates['concert_date'].split(',')]
+        date_strings = dates['concert_date'].split(', ')
         for d in date_strings:
             nieve_date = parse_datetime(d)
             aware_date = make_aware(nieve_date)
-            ConcertDate.objects.create(
+            c_date = ConcertDate.objects.create(
                 concert=concert,
                 date=aware_date
             )
+            concert.concert_date.add(c_date)
+            concert.save_revision().publish()
 
     def create_concert(self, c):
         """
         "node" : {
             "title" : "RAGE + REMEMBRANCE",
             "body" : "The Chelsea Symphony presents the season finale of RESOLUTION with a concert featuring John Corigliano's Symphony No. 1. Written in the late 1980s, the AIDS pandemic was claiming the lives of many. As the first of his large format works, the symphonic form here is used to commemorate, as the composer noted, \"my friends – those I had lost and the one I was losing.\" Partly inspired by the NAMES Project AIDS Memorial Quilt, the first movement is subtitled \"Apologue: Of Rage and Remembrance,\" and is dedicated to a pianist. The next two movements commemorate a music executive and a cellist. In the finale, a tarantella melody played by piano in a featured role and the cello line from the previous movements are juxtaposed against “a repeated pattern consisting of waves of brass chords ... [to convey] an image of timelessness.\"\n\nAlso on this finale series, TCS welcomes back two soloists to the stage for two solos for the violin: Adam von Housen performs the Mendelssohn's Violin Concerto in D minor on Saturday's concert, written by the prodigious composer when he was just 13 years old and forgotten until after his death. Sunday's matinee performance brings EJ Lee to the stage to close out our soloist season with the Beethoven Violin Concerto.\n\nBoth concerts open with In Between by Aaron Israel Levin, the winning composition from the 2018-19 TCS Composition Competition, now in its fifth year.\n\nTickets for reserved unassigned seating in a premium area are on sale at Eventbrite!\nTickets also available at the door for a suggested donation of $20. ",
-            "Concert Image" : {
+            "concert_image" : {
                 "src" : "http://localhost:8080/sites/default/files/RageRemembranceBackground2.jpg",
                 "alt" : "",
                 "title" : ""
@@ -267,26 +287,28 @@ class Command(BaseCommand):
             else "St. Paul's Church"
         redirect_path = c['path']
 
-        c_img = self.get_concert_photo_by_id(c['nid'])
-        concert_image = self.get_wagtail_image(
-            c_img['concert_img']['src'])
-
-        concert_image.focal_point_x = c_img['crop_area_X_offset']
-        concert_image.focal_point_y = c_img['crop_area_Y_offset']
-        concert_image.focal_point_width = c_img['crop_area_width']
-        concert_image.focal_point_height = c_img['crop_area_height']
-
         concert = Concert(
             title=c['title'],
             promo_copy=c['promo_copy'],
             description=c['body'],
-            concert_image=concert_image,
             venue=venue,
             legacy_id=c['nid']
         )
-        concert.full_clean()
+        concert.season = c['concert_season']
         self.concert_idx.add_child(instance=concert)
         concert.save_revision().publish()
+
+        c_img = self.get_concert_photo_by_id(c['nid'])
+        if c_img:
+            concert_image = self.get_wagtail_image(
+                c_img['concert_image']['src'])
+
+            concert_image.focal_point_x = c_img['crop_area_X_offset']
+            concert_image.focal_point_y = c_img['crop_area_Y_offset']
+            concert_image.focal_point_width = c_img['crop_area_width']
+            concert_image.focal_point_height = c_img['crop_area_height']
+            concert.concert_image = concert_image
+            concert.save()
 
         # Create a redirect
         Redirect.objects.create(old_path=c['path'], redirect_page=concert)
@@ -297,7 +319,7 @@ class Command(BaseCommand):
             self.fetch_data()
 
         concerts = (c for c in self.concerts
-                    if not Concert.objects.exists(legacy_id=c['nid']))
+                    if not Concert.objects.filter(legacy_id=c['nid']).exists())
         for c in concerts:
             print('Creating concert: ' + c['title'])
             # Create the concert
@@ -324,34 +346,34 @@ class Command(BaseCommand):
             self.fetch_data()
 
         persons = (p for p in self.people
-                  if not Person.objects.exists(legacy_id=p['uid']))
+                  if not Person.objects.filter(legacy_id=p['uid']).exists())
         for p in persons:
             print('Now creating: ' + p['name'])
             active_roster = True if p['active_roster'] == "Yes" else False
             person = Person(
-                title=p['name'],
                 first_name=p['first_name'],
                 last_name=p['last_name'],
                 biography=p['biography'],
                 active_roster=active_roster,
                 legacy_id=p['uid']
             )
-            person.full_clean()
             self.person_idx.add_child(instance=person)
             person.save_revision().publish()
 
-            instrument = InstrumentModel.objects.\
+            instrument, created  = InstrumentModel.objects.\
                 get_or_create(instrument=p['instrument'])
             person.instrument.add(instrument)
+            person.save_revision().publish()
 
             h_img = self.get_headshot_by_uid(p['uid'])
             if h_img:
-                headshot = self.get_wagtail_image(headshot['head_shot']['src'])
+                headshot = self.get_wagtail_image(h_img['head_shot']['src'])
                 headshot.focal_point_x = h_img['crop_area_X_offset']
                 headshot.focal_point_y = h_img['crop_area_Y_offset']
                 headshot.focal_point_width = h_img['crop_area_width']
                 headshot.focal_point_height = h_img['crop_area_height']
-                person.headshot.add(headshot)
+                person.headshot = headshot
+                person.save()
 
 
     def create_blogposts(self):
@@ -359,7 +381,7 @@ class Command(BaseCommand):
             self.fetch_data()
 
         posts = (p for p in self.blog_posts
-                 if not BlogPost.objects.exists(legacy_id=p['nid']))
+                 if not BlogPost.objects.filter(legacy_id=p['nid']).exists())
         for post in posts:
             print('Creating blog post: ' + post['title'])
             # This assumes that all Persons will be created first
@@ -368,11 +390,10 @@ class Command(BaseCommand):
                 title=post['title'],
                 legacy_id=post['nid'],
                 promo_copy=post['promo_copy'],
-                body=[('rich_text', RichText(post['body']))],
+                body=[('paragraph', RichText(post['body']))],
                 author=author,
                 date=parse_date(post['post_date'])
             )
-            blog_post.full_clean()
             self.blog_idx.add_child(instance=blog_post)
             blog_post.save_revision().publish()
 
@@ -384,11 +405,12 @@ class Command(BaseCommand):
             blog_img = self.get_blog_img_from_id(post['nid'])
             if blog_img:
                 blog_image = self.get_wagtail_image(blog_img['blog_image']['src'])
-                blog_image.focal_point_x = post['crop_area_X_offset']
-                blog_image.focal_point_y = post['crop_area_Y_offset']
-                blog_image.focal_point_width = post['crop_area_width']
-                blog_image.focal_point_height = post['crop_area_height']
-                blog_post.blog_image.add(blog_image)
+                blog_image.focal_point_x = blog_img['crop_area_X_offset']
+                blog_image.focal_point_y = blog_img['crop_area_Y_offset']
+                blog_image.focal_point_width = blog_img['crop_area_width']
+                blog_image.focal_point_height = blog_img['crop_area_height']
+                blog_post.blog_image = blog_image
+                blog_post.save()
 
     def handle(self, *args, **kwargs):
         self.fetch_data()
