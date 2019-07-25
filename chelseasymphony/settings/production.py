31a2537a90d78411ac366221a19f0c0ef33ae500
Nick Buonincontri <nbuonin@gmail.com>
2019-07-25 00:08:09 -0400

    DO Spaces config

--- chelseasymphony/settings/production.py
+++ chelseasymphony/settings/production.py
@@ -23,6 +23,8 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_REGION_NAME = os.environ['DO_REGION']
 AWS_S3_ENDPOINT_URL = os.environ['DO_ENDPOINT_URL']
+AWS_LOCATION = 'static'
+AWS_DEFAULT_ACL = 'public-read'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
