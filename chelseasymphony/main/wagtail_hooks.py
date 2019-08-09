787c97d1210eddce7233a4e2f5a9ce6406c890a3
Nick Buonincontri <nbuonin@gmail.com>
2019-08-09 13:27:30 -0400

    Pylint/format commit
    Implements Pylint fixes

--- chelseasymphony/main/wagtail_hooks.py
+++ chelseasymphony/main/wagtail_hooks.py
@@ -1,3 +1,6 @@
+"""Implements hooks"""
+from django.db.models import Min
+from django.http import HttpResponseRedirect
 from wagtail.core import hooks
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, modeladmin_register
@@ -6,11 +9,10 @@ from wagtail.contrib.modeladmin.mixins import ThumbnailMixin
 from .models import (
     Person, Composition, InstrumentModel, Concert, ConcertIndex
 )
-from django.db.models import Min
-from django.http import HttpResponseRedirect
 
 
 class ConcertAdmin(ModelAdmin):
+    """Creates admin page for concerts"""
     model = Concert
     menu_label = 'Concerts'
     menu_icon = 'date'
@@ -22,6 +24,7 @@ class ConcertAdmin(ModelAdmin):
 
 
 class PersonAdmin(ThumbnailMixin, ModelAdmin):
+    """Creates admin page for people"""
     model = Person
     menu_label = 'People'
     menu_icon = 'group'
@@ -35,6 +38,7 @@ class PersonAdmin(ThumbnailMixin, ModelAdmin):
 
 
 class CompositionAdmin(ModelAdmin):
+    """Creates admin page for compositions"""
     model = Composition
     menu_label = 'Compositions'
     menu_icon = 'doc-full-inverse'
@@ -46,6 +50,7 @@ class CompositionAdmin(ModelAdmin):
 
 
 class InstrumentAdmin(ModelAdmin):
+    """Creates admin page for instruments"""
     model = InstrumentModel
     menu_label = 'Instruments'
     menu_icon = 'pick'
@@ -57,43 +62,69 @@ class InstrumentAdmin(ModelAdmin):
 
 @hooks.register('construct_main_menu')
 def hide_snippets_menu_item(request, menu_items):
-      menu_items[:] = [item for item in menu_items if item.name != 'snippets']
+    """Hide the snippets menu
+
+    Admin menus for models used as snippets are rendered as model admins.
+    Therefore we don't need the snippets menu
+    """
+    menu_items[:] = [item for item in menu_items if item.name != 'snippets']
+
 
 @hooks.register('construct_explorer_page_queryset')
 def order_concert_performances(parent_page, pages, request):
     """
     This orders performance pages by the tree path order. This is the order
-    that performances are displayed, and so we want to preserve that for the editor.
+    that performances are displayed, and so we want to preserve
+    that for the editor.
     """
-    if type(parent_page) == Concert:
+    if isinstance(parent_page, Concert):
         pages = pages.order_by('path')
 
     return pages
+
 
 @hooks.register('construct_explorer_page_queryset')
 def order_concerts(parent_page, pages, request):
     """
     This orders concerts by the first concert date
     """
-    if type(parent_page) == ConcertIndex:
+    if isinstance(parent_page, ConcertIndex):
         pages = pages.annotate(
-            first_date=Min('concert__concert_date__date')).order_by('-first_date')
+            first_date=Min('concert__concert_date__date')).\
+            order_by('-first_date')
 
     return pages
 
+
 @hooks.register('after_create_page')
 def redirect_pages_to_admin(request, page):
-    if type(page) != Person:
+    """Redirect on page creation
+
+    After creating a new page, except for Person pages, redirect to
+    the explorer admin for that page, rather than the parent page in the tree.
+
+    Person pages DO get redirected to the parent 'listing' page.
+    """
+    if not isinstance(page, Person):
         return HttpResponseRedirect('/admin/pages/{}/'.format(page.id))
-    else:
-        return HttpResponseRedirect('/admin/main/person/')
+
+    return HttpResponseRedirect('/admin/main/person/')
+
 
 @hooks.register('after_edit_page')
 def redirect_pages_to_admin_edit(request, page):
-    if type(page) != Person:
+    """Redirect on page edit
+
+    After editing a page, except for Person pages, redirect to
+    the explorer admin for that page, rather than the parent page in the tree.
+
+    Person pages DO get redirected to the parent 'listing' page.
+    """
+    if not isinstance(page, Person):
         return HttpResponseRedirect('/admin/pages/{}/'.format(page.id))
-    else:
-        return HttpResponseRedirect('/admin/main/person/')
+
+    return HttpResponseRedirect('/admin/main/person/')
+
 
 modeladmin_register(ConcertAdmin)
 modeladmin_register(PersonAdmin)
