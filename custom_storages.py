53040e28892172ea669a05ba424756efff1ba66c
Nick Buonincontri <nbuonin@gmail.com>
2019-08-23 18:51:03 -0400

    Adjust storage configuration

--- custom_storages.py
+++ custom_storages.py
@@ -4,7 +4,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     bucket_name = settings.STATICFILES_BUCKET
+    default_acl = 'public-read'
+    querystring_auth = False
 
 
 class MediaStorage(S3Boto3Storage):
     bucket_name = settings.MEDIAFILES_BUCKET
+    default_acl = 'private'
+    querystring_auth = True
